619a5ba2a090daf211e5f96a43dc488f4304a8ef
Uzoy <78887981+Uzoy@users.noreply.github.com>
2022-06-07 18:04:12 +0700

    Create anime-hentai.js

---
+++ plugins/anime-hentai.js
@@ -0,0 +1,14 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, usedPrefix }) => {
+    let res = await fetch('https://api.waifu.pics/nsfw/waifu','https://api.waifu.pics/nsfw/neko')
+    if (!res.ok) throw await res.text()
+    let json = await res.json()
+    if (!json.url) throw 'Error!'
+    conn.sendButton(m.chat, 'selera yg aneh tapi ok lu yg bos', author, json.url, [['lagi;)', `${usedPrefix}hentai`]], m)
+}
+handler.help = ['hentai']
+handler.tags = ['NSFW']
+handler.command = /^(hentai)$/i
+//MADE IN ERPAN 1140 BERKOLABORASI DENGAN BTS
+export default handler
