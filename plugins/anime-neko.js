7b7886075e26dc9c616a6eafcb4561ab27a638fe
Uzoy <78887981+Uzoy@users.noreply.github.com>
2022-06-07 18:05:23 +0700

    Create anime-neko.js

---
+++ plugins/anime-neko.js
@@ -0,0 +1,14 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, usedPrefix }) => {
+    let res = await fetch('https://api.waifu.pics/sfw/neko')
+    if (!res.ok) throw await res.text()
+    let json = await res.json()
+    if (!json.url) throw 'Error!'
+    conn.sendButton(m.chat, 'Nyaa~', author, json.url, [['meong', `${usedPrefix}neko`]], m)
+}
+handler.help = ['neko']
+handler.tags = ['internet']
+handler.command = /^(neko)$/i
+//MADE IN ERPAN 1140 BERKOLABORASI DENGAN BTS
+export default handler
