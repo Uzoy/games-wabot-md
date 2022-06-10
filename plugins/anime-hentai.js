23c4ac27999afa82d6fc84854bedaa3d9484e08e
Uzoy <78887981+Uzoy@users.noreply.github.com>
2022-06-10 08:13:48 +0700

    Update anime-hentai.js

--- plugins/anime-hentai.js
+++ plugins/anime-hentai.js
@@ -10,5 +10,6 @@ let handler = async (m, { conn, usedPrefix }) => {
 handler.help = ['hentai']
 handler.tags = ['NSFW']
 handler.command = /^(hentai)$/i
+handler.private = true
 //MADE IN ERPAN 1140 BERKOLABORASI DENGAN BTS
 export default handler
