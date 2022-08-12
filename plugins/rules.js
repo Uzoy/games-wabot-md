88c68165cb18860f4096cac920cecc90698abc59
Uzoy <78887981+Uzoy@users.noreply.github.com>
2022-08-12 11:57:13 +0700

    Update rules.js

--- plugins/rules.js
+++ plugins/rules.js
@@ -4,11 +4,9 @@ let handler =  m => m.reply(`
 
 -sabtu-minggu kemungkinan diberikan waktu lebih lama karna saya tidak sekolah
 
--kalo anda dibawah 17 tahun, JANGAN PAKAI FITUR NSFW. anda walaupun udh merasa tua, kalo ortu klean liat nomor saya, nanti bisa direport saya dan nanti saya di ban.
-
 -kalo bot kadang ngetik sendiri kemungkinan owner nya bosen dan ngechat dengan anda
 
--kalo ada admin yg abuse bot dan tagall/hidetag terus, tolong panggil owner jadi saya bisa ban 
+-kalo ada admin yg abuse bot dan tagall/hidetag terus, tolong panggil owner jadi bisa di ban 
 
 -nomor bot yg dulu (+1-812-****-****) itu udh tidak bisa dipakai, jadi jangan pake nomor bot yg itu karna kemungkinan udh dipake orang dan nanti dia kebingungan knp banyak orang yg spam :v
 
@@ -19,9 +17,7 @@ let handler =  m => m.reply(`
 
 -on saturday and sunday bot will be online much longer because i don't go to school on that day
 
--if you were under 18, DON'T USE THE NSFW FEATURE, even if you think you're old enough. your parents can see your chat and report me, and then i can get banned.
-
--if bot suddenly just start typing and give you stupid messages, it's probably my stupid owner
+-if bot suddenly just start typing and give you stupid messages, it's probably the stupid owner
 
 -don't harass or disturb the owner.
 `.trim()) // Tambah sendiri kalo mau
