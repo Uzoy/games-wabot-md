702be91ceef5e7bff0fe5d730f9345dc582c4e5c
Uzoy <78887981+Uzoy@users.noreply.github.com>
2022-09-11 12:01:03 +0700

    Create anime-shinobu.js

---
+++ plugins/anime-shinobu.js
@@ -0,0 +1,14 @@
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, usedPrefix }) => {
+    let res = await fetch('https://api.waifu.pics/sfw/shinobu')
+    if (!res.ok) throw await res.text()
+    let json = await res.json()
+    if (!json.url) throw 'Error!'
+    conn.sendButton(m.chat, 'Istri kartun', author, json.url, [['next', `${usedPrefix}shinobu`]], m)
+}
+handler.help = ['shinobu']
+handler.tags = ['Anime']
+handler.command = /^(shinobu)$/i
+//MADE IN ERPAN 1140 BERKOLABORASI DENGAN BTS
+export default handler
