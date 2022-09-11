1ad362ecf780e6ce6372144fc79c204cb0b40b34
Uzoy <78887981+Uzoy@users.noreply.github.com>
2022-09-11 12:03:19 +0700

    Update anime-shinobu.js

--- plugins/anime-shinobu.js
+++ plugins/anime-shinobu.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, usedPrefix }) => {
     if (!res.ok) throw await res.text()
     let json = await res.json()
     if (!json.url) throw 'Error!'
-    conn.sendButton(m.chat, 'Istri kartun', author, json.url, [['next', `${usedPrefix}shinobu`]], m)
+    conn.sendButton(m.chat, '598 taun dia:v', author, json.url, [['next', `${usedPrefix}shinobu`]], m)
 }
 handler.help = ['shinobu']
 handler.tags = ['Anime']
